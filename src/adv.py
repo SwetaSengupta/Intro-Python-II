698af3a8141d7681139ad118611da7f96e3fefcc
sweta sengupta <ssengupta801@gmail.com>
2020-09-08 21:48:38 -0500

    Finished assignment

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
-
+from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
@@ -36,9 +37,10 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
+print("Welcome, adventurer,")
+player_name = input("Pray tell, what is your name?\n")
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(player_name, room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +51,62 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+user_input = ""
+bad_input = "Invalid Input, please try again."
+
+while user_input != 'q' and user_input != "quit":
+    print(player.location)
+    user_input = input(":").lower()
+
+    if " " in user_input:
+        verb, noun = user_input.split(" ", 1)
+        no_noun = False
+    else:
+        no_noun = True
+
+    if no_noun:
+        if user_input in ['n','s','e','w']:
+            # If the user enters a cardinal direction, attempt to move to the room there.
+
+            if user_input == 'n':
+              print("\nWalking north...\n")
+              if player.current_room.n_to is None:
+                 print("****There is no room to the North of you, select different direction.****")
+              else:
+                 player.current_room = player.current_room.n_to
+                
+            elif user_input == 's':
+              print("\nWalking south...\n")
+              if player.current_room.s_to is None:
+                 print("****There is no room to the South of you. Select a different direction.****")
+              else:
+                player.current_room = player.current_room.s_to
+            elif user_input == 'e':
+              print("\nWalking east...\n")
+              if player.current_room.e_to is None:
+                print("****There is no room to the East of you. Select a different direction.****")
+              else:
+                player.current_room = player.current_room.e_to
+            elif user_input == 'w':
+              print("\nWalking west...\n")
+              if player.current_room.w_to is None:
+                 print("****There is no room to the West of you. Select a different direction.****")
+              else:
+                player.current_room = player.current_room.w_to
+            
+            elif user_input == 'i' or user_input == 'inventory':
+                player.list_inventory()
+            elif user_input == 'l' or user_input == 'look':
+                player.location.list_items()
+            elif user_input == 'q' or user_input == "quit":
+                print("Thank you for playing!")
+            else:
+               print(bad_input)
+    else:
+        if verb == 'get':
+            player.get_item(noun)
+        elif verb == 'drop':
+            player.drop_item(noun)
+        else:
+            print(bad_input)
+    print()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,66 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, location, inventory = []):
+        self.name = name
+        self.location = location
+        self.inventory = inventory
+
+    def move(self, direction):
+        invalid = "There is nothing in that direction, please go another way."
+        if direction == 'n':
+            if self.location.n_to:
+               self.location = self.location.n_to
+            else:
+                print(invalid)
+        elif direction == 's':
+            if self.location.s_to:
+               self.location = self.location.s_to
+            else:
+                print(invalid)
+        elif direction == 'e':
+            if self.location.e_to:
+               self.location = self.location.e_to
+            else:
+                print(invalid)
+        elif direction == 'w':
+            if self.location.w_to:
+               self.location = self.location.w_to
+            else:
+                print(invalid)
+
+    def get_item(self, item):
+        selection = None
+        for i in self.location.items:
+            if i.name.lower() == item:
+                selection = i
+            else:
+                continue
+        if selection != None:
+            self.inventory.append(selection)
+            self.location.get_item(selection)
+            selection.on_take()
+        else:
+            print(f"You look around but there is no {item} in sight")
+
+    def list_inventory(self):
+        if len(self.inventory) < 1:
+            print("You are not currently carrying anything")
+        else:
+            print("Your Inventory:")
+            for item in self.inventory:
+                print(f" {item}")
+
+    def drop_item(self, item):
+        selection = None
+        for i in self.inventory:
+            if i.name.lower() == item:
+                selection = i
+            else:
+                continue
+        if selection != None:
+            self.inventory.remove(selection)
+            self.location.drop_item(selection)
+            selection.on_drop()
+        else:
+            print(f"You do not currently have {item} in your inventory.")
